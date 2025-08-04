0ab87604918771a4622d8c273cb9999f3b06e086
ngskoog <153300990+ngskoog@users.noreply.github.com>
2025-08-04 10:11:54 +1000

    Update top_secret.ps1

--- top_secret.ps1
+++ top_secret.ps1
@@ -1,3 +1,3 @@
-$src="$env:USERPROFILE\Documents\secret.txt"
-$dst="D:\payload.txt"
+$src="$env:USERPROFILE\OneDrive\Desktop\top_secret.txt"
+$dst="D:\top_secret.txt"
 if ((Test-Path $src) -and (Test-Path "D:\")) { Copy-Item $src $dst -Force }
