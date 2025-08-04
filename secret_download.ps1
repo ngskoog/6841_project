35e958f42a82b12a45822d8c2b1af3c0c1228a77
ngskoog <153300990+ngskoog@users.noreply.github.com>
2025-08-04 10:04:50 +1000

    Update secret_download.ps1

--- secret_download.ps1
+++ secret_download.ps1
@@ -1,3 +1,3 @@
-$secretPath = "$env:USERPROFILE\OneDrive\Desktop\top_secret.txt"
+$src = "$env:USERPROFILE\OneDrive\Desktop\top_secret.txt"
 $dst="D:\top_secret.txt"
 if(Test-Path $src -and (Test-Path "D:\")){Copy-Item $src $dst -Force}
